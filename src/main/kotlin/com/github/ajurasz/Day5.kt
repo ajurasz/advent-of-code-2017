959ad17d77928a2bd2894683ad798a6ff226908f
Arek Jurasz <arkadiusz.jurasz@gmail.com>
2017-12-05 21:52:41 +0100

    Replace data structure to improve performance.

--- src/main/kotlin/com/github/ajurasz/Day5.kt
+++ src/main/kotlin/com/github/ajurasz/Day5.kt
@@ -2,8 +2,8 @@ package com.github.ajurasz
 
 object Day5 {
 
-    private fun jump(numbers: List<Int>, logic: (Int, Int) -> Int): Int {
-        tailrec fun go(numbers: MutableList<Int>, position: Int = 0, jumps: Int = 1): Int {
+    private fun jump(numbers: IntArray, logic: (Int, Int) -> Int): Int {
+        tailrec fun go(numbers: IntArray, position: Int = 0, jumps: Int = 1): Int {
             val steps = numbers[position]
             if (position + steps >= numbers.size) return jumps
             return go(
@@ -11,18 +11,18 @@ object Day5 {
                     position + steps,
                     jumps.inc())
         }
-        return go(numbers.toMutableList())
+        return go(numbers)
     }
 
     @JvmStatic
     fun part1(input: String) = jump(input
             .split("\n")
-            .map { it.toInt() },
+            .map { it.toInt() }.toIntArray(),
             { _, i -> i + 1 })
 
     @JvmStatic
     fun part2(input: String) = jump(input
             .split("\n")
-            .map { it.toInt() },
+            .map { it.toInt() }.toIntArray(),
             { steps, i -> if (steps >= 3) i - 1 else i + 1 })
 }
